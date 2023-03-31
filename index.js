107133da01cc9d0651bda432c2088d95f180e122
harshpatel100 <90831761+harshpatel100@users.noreply.github.com>
2023-03-31 19:22:48 +0530

    s2

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const path = require('path');
 
 app.get('/',(req,res)=>{
   res.sendFile(__dirname+'/index.html')
 })
+const PORT = process.env.PORT || 3030;
 
+// your code
 
-app.listen(3000)
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
