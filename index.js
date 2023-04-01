4fb39503c5509e5149511cdfbbd113c444cb7ca9
harshpatel100 <90831761+harshpatel100@users.noreply.github.com>
2023-04-01 10:45:48 +0530

    v1

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const path = require('path');
-
-app.get('/',(req,res)=>{
-  res.sendFile(__dirname+'/index.html')
+const router = express.Router();
+app.use(express.static(path.join(__dirname,'public')));
+router.get('/',(req,res)=>{
+  let pathHome=path.join(__dirname+'/index.html')
+  console.log(pathHome);
+  res.sendFile(pathHome);
 })
 const PORT = process.env.PORT || 3030;
 
 // your code
-
+app.use('/',router);
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
